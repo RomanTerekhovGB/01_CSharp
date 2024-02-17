089271c4d5b59df2e1dbde787eb5f2bb7f6f198c
Roman Terekhov <terexov-roman@inbox.ru>
2024-02-18 01:33:57 +0700

    Task2

---
+++ IDZ 5/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string originalString = "HeLloWorLD";
+        string lowerCaseString = originalString.ToLower();
+
+        Console.WriteLine(lowerCaseString);
+    }
+}
